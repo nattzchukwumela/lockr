2963ff58d56a30cfb46bc3e466fb0b40e508bdd9
nattzchukwumela <chukwumelapraise@gmail.com>
2025-10-20 20:15:08 +0100

    created auth.ts and route for signin and registration using nextauth

---
+++ src/app/lib/auth.ts
@@ -0,0 +1,39 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "./prisma";
+import bcrypt from "bcrypt";
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const user = await prisma.user.findUnique({
+          where: { email: credentials?.email },
+        });
+
+        if (!user || !user.password) throw new Error("User not found");
+
+        const isValid = await bcrypt.compare(
+          credentials!.password,
+          user.password,
+        );
+
+        if (!isValid) throw new Error("Invalid credentials");
+
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
