dc4e063d2139cefda9d199a362e765cb0eef8776
nattzchukwumela <chukwumelapraise@gmail.com>
2025-10-20 21:11:33 +0100

    minor changes

---
+++ src/app/middleware.ts
@@ -0,0 +1,22 @@
+// middleware.ts
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+  function middleware(req) {
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token, // Allow only if user is authenticated
+    },
+    pages: {
+      signIn: "/auth", // ✅ valid page config
+    },
+  },
+);
+
+// Protect specific routes
+export const config = {
+  matcher: ["/dashboard/:path*", "/settings/:path*", "/2fa/:path*"],
+};
