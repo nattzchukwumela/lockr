f2183339a07f32b0e68f45163af94c6942f61478
nattzchukwumela <chukwumelapraise@gmail.com>
2025-10-21 21:17:48 +0100

    added a new field to account model `@@unique([provider, providerAccountId])` used for google auth

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -1,4 +1,3 @@
-import NextAuth from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
