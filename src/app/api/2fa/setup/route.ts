1381caab5c9cb57554f89c72c0dfd93c025d1293
nattzchukwumela <chukwumelapraise@gmail.com>
2025-10-22 09:04:49 +0100

    added better error handling to 2fa setup

--- src/app/api/2fa/setup/route.ts
+++ src/app/api/2fa/setup/route.ts
@@ -6,31 +6,51 @@ import { authenticator } from "otplib";
 import QRCode from "qrcode";
 
 export async function GET(req: Request) {
-  const session = await getServerSession(authOptions);
+  try {
+    const session = await getServerSession(authOptions);
 
-  if (!session?.user) {
-    return NextResponse.json({ err: "Unauthorised" }, { status: 400 });
+    if (!session?.user) {
+      return NextResponse.json(
+        { success: false, message: "Unauthorized" },
+        { status: 401 },
+      );
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { email: String(session.user.email) },
+    });
+
+    if (!user) {
+      return NextResponse.json(
+        { success: false, message: "User not found" },
+        { status: 404 },
+      );
+    }
+
+    const secret = authenticator.generateSecret();
+
+    if (user.email === null)
+      return NextResponse.json(
+        { err: "Unexpected error, your email is not set" },
+        { status: 400 },
+      );
+
+    const otpauth = authenticator.keyuri(user.email, "Lockr", secret);
+    const qr = await QRCode.toDataURL(otpauth);
+
+    return NextResponse.json({
+      success: true,
+      data: {
+        secret,
+        otpauth,
+        qr,
+      },
+    });
+  } catch (err) {
+    console.error("2FA Setup Error:", err);
+    return NextResponse.json(
+      { success: false, message: "Internal Server Error" },
+      { status: 500 },
+    );
   }
-
-  const user = await prisma.user.findUnique({
-    where: {
-      email: String(session?.user.email),
-    },
-  });
-
-  if (!user) {
-    return NextResponse.json({ err: "User not found" }, { status: 404 });
-  }
-
-  const secret = authenticator.generateSecret();
-  if (user.email === null)
-    return NextResponse.json({ err: "user not found" }, { status: 400 });
-  const otpauth = authenticator.keyuri(user.email, "lockr", secret);
-  const qr = await QRCode.toDataURL(otpauth);
-
-  return NextResponse.json({
-    secret,
-    otpauth,
-    qr, // This is a base64 image string for display
-  });
 }
